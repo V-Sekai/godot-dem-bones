6dedbb7c73996648ae1c1758569b2b446b3684a2
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-10-16 07:28:29 -0700

    Updates classes.

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 def can_build(env, platform):    
-    return not env["disable_3d"]
+    return False and not env["disable_3d"]
 
 
 def configure(env):
     pass
+
+def get_doc_classes():
+    return [
+        "BlendShapeBake",
+    ]
+
+def get_doc_path():
+    return "doc_classes"
